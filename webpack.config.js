8c5559efe2e1955ac9cb6ecd9ebe3e168c970c0e
Ernie Janse van Rensburg <ernie.jansevanrensburg@kiwiplan.com>
2022-03-05 06:50:59 +1300

    add image support

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,22 @@ let mode = process.env.NODE_ENV === "production" ? "production" : "development";
 
 module.exports = {
     mode: mode,
+
+    output: {
+        assetModuleFilename: "images/[hash][ext][query]"
+    },
+
     module: {
         rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset",
+                // parser: {
+                //     dataUrlCondition: {
+                //         maxSize: 30 * 1024,
+                //     }
+                // }
+            },
             {
                 test: /\.(s[ac]|c)ss$/i,
                 use: [MiniCssExtractPlugin.loader, "css-loader",  "postcss-loader","sass-loader"]
